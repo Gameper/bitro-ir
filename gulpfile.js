ffcca0ffccea039632025639c041546833a35726
Greg Jang <gregjang@gmail.com>
2019-10-26 14:57:03 +0900

    autoprefixer update

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,33 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var browserSync = require("browser-sync").create();
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task("serve", [ "sass" ], function() {
+  browserSync.init({
+    server: "./app"
+  });
 
-    browserSync.init({
-        server: "./app"
-    });
-
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch("app/scss/*.scss", [ "sass" ]);
+  gulp.watch("app/*.html").on("change", browserSync.reload);
 });
 
+var processorsArray = [
+  // snipped for brevity
+  require("autoprefixer")({ grid: true, browsers: [ "last 2 versions", "ie 10-11", "Firefox > 20" ] })
+];
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
+gulp.task("sass", function() {
+  return gulp
+    .src("app/scss/*.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss(processorsArray))
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+gulp.task("default", [ "serve" ]);
