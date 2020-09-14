42deebfa184fc7e083c39159c3f91bb12fa18bbe
Greg <gregjang@gmail.com>
2020-09-14 17:56:40 +0900

    gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
-var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var sass = require("gulp-sass");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var browserSync = require("browser-sync").create();
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
 
 // Static Server + watching scss/html files
-gulp.task("serve", [ "sass" ], function() {
+gulp.task('serve', ['sass'], function () {
   browserSync.init({
-    server: "./app"
+    server: './app',
   });
 
-  gulp.watch("app/scss/*.scss", [ "sass" ]);
-  gulp.watch("app/*.html").on("change", browserSync.reload);
+  gulp.watch('app/common/scss/*.scss', ['sass']);
+  gulp.watch('app/ch5/popup/**/*.html').on('change', browserSync.reload);
 });
 
-var processorsArray = [
-  require("autoprefixer")({ grid: true, browsers: [ "last 2 versions", "ie 10-11", "Firefox > 20" ] })
-];
+// var processorsArray = [require('autoprefixer')({ grid: true, browsers: ['last 2 versions', 'ie 10-11', 'Firefox > 20'] })];
 // Compile sass into CSS & auto-inject into browsers
-gulp.task("sass", function() {
-  return gulp
-    .src("app/scss/*.scss")
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(postcss(processorsArray))
-    .pipe(gulp.dest("app/css"))
-    .pipe(browserSync.stream());
+gulp.task('sass', function () {
+  return (
+    gulp
+      .src('app/common/scss/*.scss')
+      // .pipe(plumber())
+      .pipe(sass())
+      .pipe(postcss([autoprefixer()]))
+      .pipe(gulp.dest('app/common/css'))
+      .pipe(browserSync.stream())
+  );
 });
 
-gulp.task("default", [ "serve" ]);
+gulp.task('default', ['serve']);
